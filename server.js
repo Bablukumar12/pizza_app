532bafc95a4dab67284d56b2c389fad33a871bb1
Bablukumar12 <bablu99279728@gmail.com>
2021-05-24 09:07:04 +0530

    make home page

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const path = require('path')
 app.get('/',(req,res)=>{
   res.render('home')
 })
+app.use(express.static('public'))
 app.use(expressLayout,()=>{})
-app.set('views',path.join(__dirname,'/Resources/Views'))
+app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 
