ebbf27131e100df42350a193172378e412c1c091
Bablukumar12 <bablu99279728@gmail.com>
2021-06-09 18:09:13 +0530

    create cart page

--- server.js
+++ server.js
@@ -4,13 +4,31 @@ const port = 8000
 const ejs = require('ejs')
 const expressLayout = ('express-ejs-layouts')
 const path = require('path')
-app.get('/',(req,res)=>{
-  res.render('home')
-})
+
+
 app.use(express.static('public'))
+
 app.use(expressLayout,()=>{})
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+
+app.get('/',(req,res)=>{
+  res.render('home')
+})
+app.get('/cart',(req,res)=>{
+  res.render('customers/cart')
+})
+
+
+app.get('/login',(req,res)=>{
+  res.render('path/login')
+})
+
+
+app.get('/resister',(req,res)=>{
+  res.render('path/resister')
+})
 
 
 app.listen(port,()=>{
